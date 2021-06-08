785f2a726a541a96fecded55816c99456fa916c2
cy <1837060380@qq.com>
2021-06-08 15:51:39 +0800

    update

---
+++ CYTool/Classes/GlobalEnum.swift
@@ -0,0 +1,37 @@
+//
+//  GlobalEnum.swift
+//  JiMeiCloud
+//
+//  Created by cy on 2021/3/22.
+//
+
+
+enum OrderDetailType {
+    case OrderComplete // 已完成/
+    case OrderWaitPay // 待支付/
+    case OrderWaitUse //待使用/
+    case OrderCancel // 已取消/
+    case OrderRefund // 已退款/
+    case OrderTimeout // 已超时/(下单后未支付,订单超时)
+    case OrderOverdue // 已过期/(下单后未使用,订单过期)
+    
+}
+
+extension OrderDetailType {
+    var displayName: String{
+        switch self {
+        case .OrderComplete:
+            return "已完成"
+        case .OrderWaitPay:
+            return "待付款"
+        case .OrderWaitUse:
+            return "待使用"
+        case .OrderCancel,.OrderTimeout:
+            return "已取消"
+        case .OrderRefund:
+            return "已退款"
+        case .OrderOverdue:
+            return "已过期"
+        }
+    }
+}
---
+++ Example/Pods/Target Support Files/CYTool/CYTool-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CYTool : NSObject
+@end
+@implementation PodsDummy_CYTool
+@end
